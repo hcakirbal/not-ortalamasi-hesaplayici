e2fc97430ab2c49de6a1551971cb776b2c1bfd02
hasan <hasancakirbal2003@gmail.com>
2023-07-23 17:31:02 +0300

    Birden fazla ders ortalaması veriyor

--- project.py
+++ project.py
@@ -1,6 +1,9 @@
-not_limit = int(input("kaç not gireceksiniz? \n"))
-notlar = 0
-for i in range(0, not_limit):
-  notlar += int(input("notu girin \n"))
-  if i == (not_limit-1):
-    print("ortalamanız:\n",notlar/not_limit)
+ders_limit = int(input("kaç ders gireceksiniz? \n"))
+for n in range(0, ders_limit):
+  notlar = 0
+  ders_ismi = input("dersin ismini girin \n")
+  not_limit = int(input("kaç not gireceksiniz? \n"))
+  for i in range(0, not_limit):
+    notlar += int(input("notu girin \n"))
+    if i == (not_limit-1):
+      print(ders_ismi,"ortalamanız:\n",notlar/not_limit)
