65e91ff81a8ae151209019a3e32d68d50a50369a
hasan <hasancakirbal2003@gmail.com>
2023-07-23 18:33:44 +0300

    Tamamlandı

--- project.py
+++ project.py
@@ -1,5 +1,7 @@
 ders_limit = int(input("kaç ders gireceksiniz? \n"))
 katsayi_array = []
+ort_array = []
+toplam = 0
 for n in range(0, ders_limit):
   notlar = 0
   notlar_array = []
@@ -12,4 +14,10 @@ for n in range(0, ders_limit):
     notlar = int(input("notu girin \n"))
     notlar_array.append(notlar)
     if i == (not_limiti-1):
-      print(ders_ismi,"ortalamanız:\n",sum(notlar_array)/not_limiti)
+      ort_array.append(sum(notlar_array)/(not_limiti))
+      print(ders_ismi,"ortalamanız:\n", sum(notlar_array)/(not_limiti))
+  if n == (ders_limit-1):
+    for m in range(0, ders_limit):
+      toplam += ort_array[m] * katsayi_array[m]
+    genel_ortalama = toplam / sum(katsayi_array)
+    print("Genel ortalamanız: \n", genel_ortalama)
